7d185078e2bd3bf65bde215bebf5f287eb6f22af
Abhinav Jadhav <Abhinavjadhav591@gmail.com>
2026-02-22 15:44:28 +0530

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	Day01/Main.java

---
+++ Day01/simpleIntrest.java
@@ -0,0 +1,20 @@
+package Day01;
+import java.util.Scanner;
+
+public class simpleIntrest {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+    System.out.println("Enter Principle Amount (integer):");
+    int principal = scanner.nextInt();
+    System.out.println("Enter Rate of intrest (integer):");
+    int rate = scanner.nextInt();
+    System.out.println("Enter Time in Years (integer):");
+    int time = scanner.nextInt();
+
+    int interest = (principal * rate * time) / 100;
+    System.out.println("The simple intrest is:"+interest);
+
+    }
+}
+
