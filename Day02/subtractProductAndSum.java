570a029fa69b3d917442aacde7d179949c727f30
Abhinav Jadhav <Abhinavjadhav591@gmail.com>
2026-02-23 13:43:02 +0530

    Day-02 Program to Subtract the Product and Sum of Digits of an Integer

---
+++ Day02/subtractProductAndSum.java
@@ -0,0 +1,33 @@
+package Day02;
+
+import java.util.Scanner;
+
+// Subtract the Product and Sum of Digits of an Integer
+public class subtractProductAndSum {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter number: ");
+        int n = sc.nextInt();
+
+        int product = 1;
+        int sum = 0;
+
+        while (n > 0) {
+            int temp = n % 10;
+
+            product = product * temp;
+            sum = sum + temp;
+
+            n = n / 10;
+        }
+
+        int result = product - sum;
+
+        System.out.println("Result = " + result);
+
+        sc.close();
+    }
+}
