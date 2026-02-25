4bca436b47b14197afdd423411e8caf68e3d7eb2
AbhinavJadhav001 <abhinavjadhav591@gmail.com>
2026-02-25 16:46:04 +0530

    Day01-Find factorial od a number

---
+++ Day04/FactorialProgram.java
@@ -0,0 +1,16 @@
+package Day04;
+import java.util.Scanner;
+public class FactorialProgram {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a number: ");
+        int num = scanner.nextInt();
+        int factorial = 1;
+                for (int i = 1; i<=num ; i++){
+                    factorial = factorial*i ;
+            }
+                System.out.println("Factorial is:" + factorial);
+                scanner.close();
+        }
+    }
+
